4377b360088b20f86c93f893b9d56b3c910c9dd8
Anshul Rathore <anshul21rathore@gmail.com>
2024-10-10 16:58:03 +0530

    Fixed installation path

--- cmake/InstallTargets.cmake
+++ cmake/InstallTargets.cmake
@@ -1,14 +1,14 @@
 # install
-install(DIRECTORY ${OrbbecSDK_INCLUDE_DIRS}/ DESTINATION include)
-install(DIRECTORY ${ORBBEC_CONFIG_DIR}/ DESTINATION lib)
-install(DIRECTORY ${ORBBEC_CONFIG_DIR}/ DESTINATION bin)
-install(DIRECTORY ${ORBBEC_EXAMPLES_DIR}/ DESTINATION examples)
-install(DIRECTORY ${ORBBEC_DOCS_DIR}/ DESTINATION doc)
+install ( DIRECTORY ${OrbbecSDK_INCLUDE_DIRS}/ DESTINATION include )
+install ( DIRECTORY ${ORBBEC_CONFIG_DIR}/ DESTINATION lib )
+install ( DIRECTORY ${ORBBEC_CONFIG_DIR}/ DESTINATION bin )
+install ( DIRECTORY ${ORBBEC_EXAMPLES_DIR}/ DESTINATION examples )
+install ( DIRECTORY ${ORBBEC_DOCS_DIR}/ DESTINATION doc )
 
-if (UNIX)
-    install(FILES ${ORBBEC_SCRIPT_DIR}/99-obsensor-libusb.rules DESTINATION scripts/)
-    install(FILES ${ORBBEC_SCRIPT_DIR}/install_udev_rules.sh DESTINATION scripts/)
-    install(DIRECTORY
+if ( UNIX )
+    install ( FILES ${ORBBEC_SCRIPT_DIR}/99-obsensor-libusb.rules DESTINATION scripts/ )
+    install ( FILES ${ORBBEC_SCRIPT_DIR}/install_udev_rules.sh DESTINATION scripts/ )
+    install ( DIRECTORY
         ${OrbbecSDK_LIBS_DIR}/
         DESTINATION lib
         FILES_MATCHING PATTERN "*.so"
@@ -17,27 +17,27 @@ if (UNIX)
         PATTERN "*.*.dylib"
     )
 
-    if (INSTALL_LIBUDEV)
-        install(DIRECTORY
+    if ( INSTALL_LIBUDEV )
+        install ( DIRECTORY
             ${OrbbecSDK_LIBS_DIR}/
             DESTINATION lib
             FILES_MATCHING PATTERN "libudev.so"
             PATTERN "libudev.so.*"
         )
     endif ()
-elseif (WIN32)
-    install(DIRECTORY
+elseif ( WIN32 )
+    install ( DIRECTORY
         ${ORBBEC_SCRIPT_DIR}/
         DESTINATION scripts
         FILES_MATCHING PATTERN "obsensor_metadata_win10*"
     )
-    install(DIRECTORY
+    install ( DIRECTORY
         ${OrbbecSDK_LIBS_DIR}/
         DESTINATION bin
         FILES_MATCHING PATTERN "*.dll"
         PATTERN ".lib"
     )
-    install(DIRECTORY
+    install ( DIRECTORY
         ${OrbbecSDK_LIBS_DIR}/
         DESTINATION lib
         FILES_MATCHING
@@ -46,8 +46,4 @@ elseif (WIN32)
     )
 endif ()
 
-# configure_file(${CMAKE_CURRENT_SOURCE_DIR}/cmake/OrbbecSDKConfig.cmake.in "${CMAKE_CURRENT_BINARY_DIR}/OrbbecSDKConfig.cmake" @ONLY)
-
-# install(FILES "${CMAKE_CURRENT_BINARY_DIR}/OrbbecSDKConfig.cmake" DESTINATION lib/)
-
-include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/Packing.cmake)
+include ( ${CMAKE_CURRENT_SOURCE_DIR}/cmake/Packing.cmake )
